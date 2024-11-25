a7d378081a98925c1e3f68424740aec6cf1b4860
kahanscious <kahanscious@gmail.com>
2024-11-25 17:44:10 -0500

    video 2

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,18 +1,41 @@
 class_name Player extends CharacterBody2D
 
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 @export var speed: float = 200.0
 @export var gravity: float = 980.0
 
-
+var direction: float
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		
+	handle_movement(delta)
+	update_animations()
+	move_and_slide()
+	
 
-	var direction: float = Input.get_axis("ui_left", "ui_right")
+	
+
+
+func handle_movement(delta: float) -> void:
+	direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * speed
+		sprite.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
-
-	move_and_slide()
+	
+func update_animations() -> void:
+	if not is_on_floor():
+		if velocity.y < 0:
+			animation_player.play("jump")
+		else:
+			animation_player.play("fall")
+	else:
+		if velocity.x != 0:
+			animation_player.play("run")
+		else:
+			animation_player.play("idle")
