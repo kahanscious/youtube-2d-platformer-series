a5d686858ea8afd93c586ed5e2de46732e2049f0
kahanscious <kahanscious@gmail.com>
2024-11-27 16:05:31 -0500

    video 8

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -4,13 +4,21 @@ class_name Player extends CharacterBody2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var state_machine: StateMachine = $StateMachine
 @onready var gun_muzzle: Marker2D = $GunMuzzle
+@onready var hitbox: Hitbox = $Hitbox
 
+@export_category("Physics")
 @export var gravity: float = 980.0
 @export var speed: float = 200.0
 @export var air_speed: float = 150.0
 @export var air_acceleration: float = 0.1
 @export var jump_force: float = -400.0
+
+@export_category("Attack")
 @export var max_bullets: int = 3
+
+@export_category("Health")
+@export var max_health: int = 5
+@export var current_health: int = 5
 
 var direction: float
 
@@ -18,7 +26,9 @@ const GUN_MUZZLE_OFFSET: int = 26
 
 
 func _ready() -> void:
+	print("Player current health: ", current_health)
 	state_machine.configure(self)
+	hitbox.take_damage.connect(_on_take_damage)
 
 
 func _process(_delta: float) -> void:
@@ -44,3 +54,8 @@ func can_shoot() -> bool:
 func set_player_direction() -> void:
 	sprite.flip_h = direction < 0
 	gun_muzzle.position.x = -GUN_MUZZLE_OFFSET if direction < 0 else GUN_MUZZLE_OFFSET
+
+
+func _on_take_damage(amount: int) -> void:
+	current_health -= amount
+	print("Player current health: ", current_health)
