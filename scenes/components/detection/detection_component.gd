66b05845ffbbb563b3a3bcc0b5abc50ce0e5f51a
kahanscious <kahanscious@gmail.com>
2024-12-08 12:40:05 -0500

    tweaks

--- scenes/components/detection/detection_component.gd
+++ scenes/components/detection/detection_component.gd
@@ -6,11 +6,9 @@ signal player_exited
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
-		print("player entered")
 		player_entered.emit()
 
 
 func _on_body_exited(body: Node2D) -> void:
 	if body is Player:
-		print("player exited")
 		player_exited.emit()
