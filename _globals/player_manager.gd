30e3d59ee5caa36a68a5892336da09e3863b968d
kahanscious <kahanscious@gmail.com>
2024-12-10 18:01:20 -0500

    level manager fixes

--- _globals/player_manager.gd
+++ _globals/player_manager.gd
@@ -7,3 +7,13 @@ var player: Player
 
 func _ready() -> void:
 	player = PLAYER.instantiate()
+
+
+func level_setup(parent: Node2D) -> void:
+	if player.get_parent():
+		player.get_parent().remove_child(player)
+	parent.add_child(player)
+
+
+func level_cleanup(parent: Node2D) -> void:
+	parent.remove_child(player)
--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -4,5 +4,12 @@ class_name Level extends Node2D
 func _ready() -> void:
 	RenderingServer.set_default_clear_color(Color("00003c"))
 
-	add_child(PlayerManager.player)
+	PlayerManager.level_setup(self)
+	LevelManager.level_load_started.connect(_level_cleanup)
+
 	PlayerManager.player.global_position = %PlayerSpawn.global_position
+
+
+func _level_cleanup() -> void:
+	PlayerManager.level_cleanup(self)
+	queue_free()
